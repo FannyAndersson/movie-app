3556b30c7403a15e1e9aa4dccc74d892b45ef76c
Fanny <fanny_andersson@hotmail.com>
2020-04-07 17:25:49 +0200

    Import bootrstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './sass/main.scss';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            kfdsj
+          </Col>
+          <Col>
+            ldspaåo
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
