e00da028b0827e0771049446a9cd94be7325a89d
Fanny <fanny_andersson@hotmail.com>
2020-04-28 21:08:44 +0200

    Add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './NavBar';
+import Search from './Search';
+
+const Header = () => {
+
+
+
+  return <header className="App-header">
+      <NavBar />
+      <Search />
+    </header>;
+};
+
+export default Header;
