9eb3dfdc73967a0f2000f8f0e67709ea0d8ef140
Fanny <fanny_andersson@hotmail.com>
2020-04-28 21:35:50 +0200

    Add startpage

---
+++ src/components/Startpage/Startpage.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import NowPlayingMovies from './NowPlayingMovies';
+import TopRankedMovies from './TopRankedMovies';
+
+const StartPage = () => {
+
+
+  return (
+    <div>
+        <NowPlayingMovies />
+        <TopRankedMovies />
+    </div>
+  );
+};
+
+export default StartPage;
