f10ca4b0f5dd2292f4e29fe6aee89bc03950eb3b
Fanny <fanny_andersson@hotmail.com>
2020-04-28 20:56:19 +0200

    Add App

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../sass/source.scss';
+// import { Container, Row, Col } from 'react-bootstrap';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+import MostPopular from './MostPopular/MostPopular';
+import UpcomingMovies from './UpcomingMovies/UpcomingMovies';
+import {MovieProvider} from './MovieContext';
+import Header from './Header';
+import MovieDetails from './MovieDetails/MovieDetails';
+import NowPlayingMovies from './Startpage/NowPlayingMovies';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import { useRoutes, A } from 'hookrouter';
+
+
+console.log(process.env.REACT_APP_TMDB_KEY);
+
+
+
+// const routes = {
+//   '/movie/:id': ({id}) => <MovieDetails id={id} />,
+//   '/latest-movies': ( ) => <MovieList />
+// };
+// console.log(routes);
+
+function App() {
+
+// const routeResult = useRoutes(routes);
+
+
+
+  return <MovieProvider>
+  <BrowserRouter>
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={NowPlayingMovies} />
+          <Route path="/kommande-filmer" exact component={UpcomingMovies} />
+          <Route path='/populara-filmer' exact component={MostPopular} />
+          <Route path="/movie/:id" exact component={MovieDetails} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
+    </MovieProvider>
+}
+
+
+export default App;
